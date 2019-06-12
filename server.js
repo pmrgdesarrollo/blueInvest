4ab7c09477d8f078ad44956c56028bab8035b22f
pmrg.desarrollo <pmrg.desarrollo@gmail.com>
2019-06-12 16:11:02 -0500

    vamospordios3

--- server.js
+++ server.js
@@ -71,12 +71,19 @@ server.listen( 3000, () => { console.log('puerto 3000:\x1b[33m%s\x1b[0m', 'Onlin
 
 const path = require('path');
 
-const app2 = express();
+const expressFront = express();
 
-app2.use(express.static(__dirname + '/dist/abakoadv'));
+expressFront.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 
-app2.get('/*', (req, res) => {
+expressFront.use(express.static(__dirname + '/dist/abakoadv'));
+
+expressFront.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/abakoadv/index.html'))
 });
 
-app2.listen(process.env.PORT || 8080);
+expressFront.listen(process.env.PORT || 8080);
